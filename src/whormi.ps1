f35f2cf10841c872c9901df4c0bd7fa7d34cb9c2
Ed Elliott <goeddieuk@gmail.com>
2018-10-03 06:29:33 +0100

    draft v1

---
+++ src/whormi.ps1
@@ -0,0 +1,115 @@
+
+Function Get-VMMetadata {
+    [CmdletBinding()]    
+    param(
+        $apiVersion = '2017-08-01',
+        $hostName = '169.254.169.254',
+        $uri = 'metadata/instance'
+    )   
+
+    $url = "http://$hostName/$uri?api-version=$apiVersion"
+
+    
+    class NetworkInterface {
+        [string]$ip4PrivateAddress
+        [String]$ip4PublicAddress
+        [String]$ip4SubnetAddress        
+        [String]$ip4SubnetPrefix
+
+        [String]$macAddress
+
+    }
+
+    class InstanceMetadata {
+    
+        [String]$location
+        [String]$name
+        [String]$resourceGroupName
+        [String]$subscriptionId
+        [String]$vmId
+        [String]$vmSize
+        [String]$tags
+        [String]$version
+    
+        [String]$offer
+        [String]$osType
+        [String]$publisher
+        [String]$sku
+        [String]$apiVersion
+
+
+        [String]$placementGroupID
+        [String]$platformFaultDomain
+        [String]$platformUpdateDomain
+
+        [NetworkInterface[]]$NetworkInterfaces
+
+        $RawResponse
+    }
+
+    $json = Invoke-MetadataService -URL $url
+    $notes = $json | ConvertFrom-Json
+    
+    [InstanceMetadata]$metadata = New-Object InstanceMetadata
+    $metadata.location = $notes.compute.location
+    $metadata.name = $notes.compute.name
+    $metadata.offer = $notes.compute.offer
+    $metadata.osType = $notes.compute.osType
+    $metadata.placementGroupId = $notes.compute.placementGroupId
+    $metadata.platformFaultDomain = $notes.compute.platformFaultDomain
+    $metadata.platformUpdateDomain = $notes.compute.platformUpdateDomain
+    $metadata.publisher = $notes.compute.publisher
+    $metadata.resourceGroupName = $notes.compute.resourceGroupName
+    $metadata.sku = $notes.compute.sku
+    $metadata.subscriptionId = $notes.compute.subscriptionId
+    $metadata.tags = $notes.compute.tags
+    $metadata.version = $notes.compute.version
+    $metadata.vmId = $notes.compute.vmId
+    $metadata.vmSize = $notes.compute.vmSize
+
+    $metadata.NetworkInterfaces += ($notes.network.interface | ForEach-Object {
+            $interface = $_
+        
+            Write-Verbose ($interface.ipv4.subnet[0]).address
+            Write-Verbose $interface.ipv4.subnet[0].prefix
+            Write-Verbose $interface.ipv4.ipAddress.privateIPAddress
+            Write-Verbose $interface.ipv4.ipAddress.publicIPAddress
+            Write-Verbose $interface.macAddress
+        
+            [NetworkInterface]$nic = New-Object NetworkInterface
+            $nic.macAddress = $interface.macAddress
+            $nic.ip4PublicAddress = $interface.ipv4.ipAddress.publicIPAddress
+            $nic.ip4PrivateAddress = $interface.ipv4.ipAddress.privateIPAddress
+            $nic.ip4SubnetPrefix = $interface.ipv4.subnet[0].prefix
+            $nic.ip4SubnetAddress = $interface.ipv4.subnet[0].address
+
+            $nic
+        }
+    )
+
+    $metadata.RawResponse = $notes
+        
+    $metadata
+
+}
+
+
+
+Function Invoke-MetadataService {
+    [CmdletBinding()]
+    param(    
+        [String]$URL
+    )
+
+    Write-Verbose "Invoke-MetadataService:Requesting:URL: '$URL'"
+
+    $response = Invoke-WebRequest -Uri $URL -UseBasicParsing -Headers @{"Metadata" = "true"}
+    if ($response -eq $null) {
+        Write-Error "Unable to get a response, no metadata for you"
+    }
+    else {
+        Write-Verbose "Invoke-MetadataService:Success"
+        $response.content
+    }
+    
+}
---
+++ test/Get-VMMetadata.Tests.ps1
@@ -0,0 +1,15 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+
+Describe "Get-VMMetadata" {
+    It "works" {
+        . "$here/../src/whormi.ps1"
+        Mock Invoke-MetadataService {
+            Get-Content "$here/instance.json"
+        }
+
+        
+        $metadata = Get-VMMetadata -verbose
+        $metadata.location | Should Be 'westeurope'
+    }
+}
